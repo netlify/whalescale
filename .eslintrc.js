95fc66e9be6c56531ddb29005ecd96193f6e3014
Vivian Brown <vivian@netlify.com>
2021-01-05 14:02:23 -0800

    Add eslint config and lint

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+const { overrides } = require('@netlify/eslint-config-node')
+
+module.exports = {
+  extends: '@netlify/eslint-config-node',
+  rules: {},
+  overrides: [...overrides],
+}
--- index.js
+++ index.js
@@ -1,50 +1,52 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const childProcess = require('child_process')
+const util = require('util')
 
-const util = require('util');
-const exec = util.promisify(require('child_process').exec);
+const exec = util.promisify(childProcess.exec)
 
+const core = require('@actions/core')
+const github = require('@actions/github')
 
-async function run(defaultTag, currentTag) {
+const run = async function (defaultTag, currentTag) {
   try {
     const [oldSize, newSize] = await Promise.all([getDefaultSize(defaultTag), getNewSize(currentTag)])
 
     const summary = `
 Default image size: ${oldSize}
 New image size: ${newSize}`
-
-    let title
-    const change = (newSize/oldSize-1)*100
-    if (change > 0) {
-      title = `⚠️ ${change.toFixed(3)}% larger than ${defaultTag}`
-    } else if (change < 0) {
-      title = `🎉 ${Math.abs(change.toFixed(3))}% smaller than ${defaultTag}`
-    } else {
-      title = `🐋 No change`
-    }
-
+    const title = buildTitle(newSize, oldSize, defaultTag)
     await createCheck(title, summary)
   } catch (error) {
-    core.setFailed(error.message);
+    core.setFailed(error.message)
   }
-} 
+}
 
-async function getDefaultSize(tag) {
-  await exec('docker pull ' + tag);
-  const { stdout } = await exec('docker image inspect ' + tag + ' --format="{{.Size}}"');
+const getDefaultSize = async function(tag) {
+  await exec(`docker pull ${tag}`);
+  const { stdout } = await exec(`docker image inspect ${tag} --format="{{.Size}}"`)
   return stdout
 }
 
-async function getNewSize(tag) {
-  if (tag === undefined) {
-    tag = "$GITHUB_SHA"
-    await exec('docker build . -t ' + tag);
+const getNewSize = async function(prebuiltTag) {
+  if (prebuiltTag === undefined) {
+    await exec('docker build . -t $GITHUB_SHA')
   }
-  const { stdout } = await exec('docker image inspect ' + tag + ' --format="{{.Size}}"');
+  const tag = prebuiltTag || '$GITHUB_SHA'
+  const { stdout } = await exec(`docker image inspect ${tag} --format="{{.Size}}"`)
   return stdout
 }
 
-async function createCheck(title, summary) {
+const buildTitle = function(newSize, oldSize, defaultTag) {
+  const change = (newSize/oldSize-1)*100
+  if (change > 0) {
+    return `⚠️ ${change.toFixed(3)}% larger than ${defaultTag}`
+  }
+  if (change < 0) {
+    return `🎉 ${Math.abs(change.toFixed(3))}% smaller than ${defaultTag}`
+  }
+  return `🐋 No change`
+}
+
+const createCheck = async function(title, summary) {
   const token = core.getInput('token')
   const octokit = github.getOctokit(token)
   await octokit.checks.create(
@@ -54,11 +56,12 @@ async function createCheck(title, summary) {
       head_sha: github.context.sha,
       conclusion: 'neutral',
       output: {
-        title: title,
-        summary: summary
+        title,
+        summary
       }
     }
   )
 }
 
-run(core.getInput('defaultTag'), core.getInput('currentTag')).then((val) => console.log(val))
+run(core.getInput('defaultTag'), core.getInput('currentTag'))
+  .catch((error) => console.log(error))
