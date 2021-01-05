1393014942bd033d781dbd087dcaa6c0be3a6964
Vivian Brown <vivian@netlify.com>
2021-01-05 14:13:47 -0800

    Run prettier

--- index.js
+++ index.js
@@ -20,13 +20,13 @@ New image size: ${newSize}`
   }
 }
 
-const getDefaultSize = async function(tag) {
-  await exec(`docker pull ${tag}`);
+const getDefaultSize = async function (tag) {
+  await exec(`docker pull ${tag}`)
   const { stdout } = await exec(`docker image inspect ${tag} --format="{{.Size}}"`)
   return stdout
 }
 
-const getNewSize = async function(prebuiltTag) {
+const getNewSize = async function (prebuiltTag) {
   if (prebuiltTag === undefined) {
     await exec('docker build . -t $GITHUB_SHA')
   }
@@ -35,8 +35,8 @@ const getNewSize = async function(prebuiltTag) {
   return stdout
 }
 
-const buildTitle = function(newSize, oldSize, defaultTag) {
-  const change = (newSize/oldSize-1)*100
+const buildTitle = function (newSize, oldSize, defaultTag) {
+  const change = (newSize / oldSize - 1) * 100
   if (change > 0) {
     return `⚠️ ${change.toFixed(3)}% larger than ${defaultTag}`
   }
@@ -46,22 +46,19 @@ const buildTitle = function(newSize, oldSize, defaultTag) {
   return `🐋 No change`
 }
 
-const createCheck = async function(title, summary) {
+const createCheck = async function (title, summary) {
   const token = core.getInput('token')
   const octokit = github.getOctokit(token)
-  await octokit.checks.create(
-    {
-      ...github.context.repo,
-      name: 'Docker image size',
-      head_sha: github.context.sha,
-      conclusion: 'neutral',
-      output: {
-        title,
-        summary
-      }
-    }
-  )
+  await octokit.checks.create({
+    ...github.context.repo,
+    name: 'Docker image size',
+    head_sha: github.context.sha,
+    conclusion: 'neutral',
+    output: {
+      title,
+      summary,
+    },
+  })
 }
 
-run(core.getInput('defaultTag'), core.getInput('currentTag'))
-  .catch((error) => console.log(error))
+run(core.getInput('defaultTag'), core.getInput('currentTag')).catch((error) => console.log(error))
